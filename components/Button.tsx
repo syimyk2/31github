76d93a510d4093828396399818b3892dd224b74b
syimyk2 <syimyk.jetibaev99@gmail.com>
2023-12-03 23:58:45 +0600

    feat: getapp

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,12 +5,15 @@ type ButtonProps = {
 	type: 'button' | 'submit'
 	icon?: string
 	variant: string
+	full?: boolean
 }
 
-const Button = ({ title, type, icon, variant }: ButtonProps) => {
+const Button = ({ title, type, icon, variant, full }: ButtonProps) => {
 	return (
 		<button
-			className={`flexCenter gap-3 rounded-full border ${variant}`}
+			className={`flexCenter gap-3 rounded-full border ${variant} ${
+				full && 'w-full'
+			}`}
 			type={type}
 		>
 			{icon && <Image src={icon} alt={title} width={24} height={24} />}
